35b802f750c83daf5643190e331da8a4ed5d6caa
gilmar soares <linux.soares@gmail.com>
2019-06-26 01:04:43 -0300

    Bug find friends

--- src/nukr/logic.clj
+++ src/nukr/logic.clj
@@ -21,14 +21,14 @@
   (let [user (get data id)]
     (get user id)))
 
-(defn recommendations [data recommendation ids remove-id]
+(defn recommendations [data recommendation ids]
   (if (not-empty ids)
-    (let [friends (get-friends data (first ids))
-          find (remove #{remove-id} (distinct (concat (rest ids) recommendation)))]
-      (recur data (concat friends recommendation) find (concat (first ids) remove-id)))
+    (let [friends (get-friends data (first ids))]
+      (recur data (distinct (concat friends recommendation ids)) (rest ids)))
     recommendation))
 
 (defn recommendation [data id]
-  (recommendations data nil (get-friends data id) nil))
+  (let [recommendation (recommendations data nil (get-friends data id))]
+    (remove #{id} (remove #{(get-friends data id)} recommendation))))
 
 (def data {1 {1 '(2), :id 1}, 2 {2 '(1 3), :id 2}, 3 {3 '(1 4 5), :id 3}})
